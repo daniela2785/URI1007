45bf9edbea0f3c7434177777db18d0ff9a4b7713
daniela marques <dd978452@gmail.com>
2023-01-19 14:05:48 -0300

    Adicionar arquivos de projeto.

---
+++ URI1007/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+namespace URI { 
+class URI
+{
+
+    static void Main(string[] args)
+    {
+            /*int A, B, C, D, diferença;
+
+            A= int.Parse(Console.ReadLine());
+            B= int.Parse(Console.ReadLine());
+            C= int.Parse(Console.ReadLine());
+            D= int.Parse(Console.ReadLine());
+
+            diferença= ((A * B) - (C * D));
+
+            Console.WriteLine("DIFERENCA = " + diferença);
+            */
+
+            int numeroFuncionario, horasTrabalhadas;
+            double ValorHora, salario;
+
+            numeroFuncionario = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            horasTrabalhadas = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ValorHora= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = (horasTrabalhadas * ValorHora);
+
+            Console.WriteLine("NÚMERO = " + numeroFuncionario);
+            Console.WriteLine("SALÁRIO = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+}
+}
